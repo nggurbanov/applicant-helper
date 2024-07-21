e63a18ff4e936b3e45b0ce987ed41e0dd2e5ad6c
Denis Indenbom <59681620+denisindenbom@users.noreply.github.com>
2024-07-21 12:42:37 +0300

    Implemented dialog mode for tg bot (maybe not work)

--- artem.py
+++ artem.py
@@ -1,23 +1,28 @@
 from config import *
 import tools
 from keyboard import get_keyboard
-# from aiogram.exceptions import TelegramNetworkError
+
+dialog_mode = False
+
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
     await message.answer("Привет! Просто задай свой вопрос, а я попробую тебе помочь.")
 
+
 @dp.message(Command("refresh"))
 async def command_refresh_handler(message: Message) -> None:
     await tools.refresh()
     await message.answer("Актуальные данные скачаны из таблицы!")
-    
+
+
 @dp.chat_member(ChatMemberUpdatedFilter(IS_NOT_MEMBER >> IS_MEMBER))
 async def on_user_join(event: ChatMemberUpdated, bot: Bot):
     await asyncio.sleep(1)
     await bot.send_chat_action(event.chat.id, action="typing")
     await asyncio.sleep(4)
     await event.answer("Привет! Ты новенький, представься)")
+
 
 @dp.message(Command("summarize"))
 async def command_summarize_handler(message: Message) -> None:
@@ -28,23 +33,34 @@ async def command_summarize_handler(message: Message) -> None:
         await message.reply(summary)
 
 
+@dp.message(Command("dialog"))
+async def command_dialog_handler(message: Message) -> None:
+    global dialog_mode
+
+    if str(message.chat.id) == UNDERGROUND_CHAT_ID:
+        dialog_mode = not dialog_mode
+
+        await message.reply('Ура, теперь я полноценный участник беседы!' if dialog_mode else 'Всем пока!')
+
+
 @dp.message()
 async def chat_message_handler(message: Message, state: FSMContext) -> None:
-    global underground_chat_context
+    global dialog_mode
+
     if message.chat.type == 'group' or message.chat.type == 'supergroup':
         if await tools.mentioned(message.text):
-            reply = await tools.formatted_reply(message.text)
+            reply = await tools.formatted_reply(message.text, dialog_mode)
             await message.reply(reply)
             await tools.update_undergound_context(reply, "Артем Макаров")
-        
+
         if "&&" in message.text and await tools.is_admin(message):
-                keywords = message.text.replace("&& ", "").replace("&&", "")
-                answer = message.reply_to_message.text
-                await tools.add_new_question(keywords, answer)
-                await tools.refresh()
-                await message.reply("Добавлено!")
-        
-        if message.text  == "!a" or message.text == "!а" :
+            keywords = message.text.replace("&& ", "").replace("&&", "")
+            answer = message.reply_to_message.text
+            await tools.add_new_question(keywords, answer)
+            await tools.refresh()
+            await message.reply("Добавлено!")
+
+        if message.text == "!а":
             question = message.reply_to_message.text
             reply = await tools.formatted_reply(question)
             await message.reply_to_message.reply(reply)
@@ -52,8 +68,10 @@ async def chat_message_handler(message: Message, state: FSMContext) -> None:
     else:
         reply = await tools.reply(message.text)
         await message.reply(reply, reply_markup=get_keyboard())
-        await state.update_data({"text": message.text})
+        await state.update_data({"text":message.text})
+
     await tools.update_undergound_context(message)
+
 
 @dp.callback_query(F.data == "ASK")
 async def handle_answer_quality(callback: CallbackQuery, bot: Bot, state: FSMContext) -> None:
@@ -62,22 +80,29 @@ async def handle_answer_quality(callback: CallbackQuery, bot: Bot, state: FSMCon
     message = Text(
         Bold("Кураторы!\n\nАноним задает вопрос:\n"),
         ExpandableBlockQuote(text))
+
     await bot.send_message(UNDERGROUND_CHAT_ID, **message.as_kwargs())
-    await callback.message.edit_text(f"Твой вопрос отправлен!\n Жди ответа в чате по [ссылке]({UNDERGROUND_CHAT_INVITE}).", parse_mode=ParseMode.MARKDOWN, link_preview_options=LinkPreviewOptions(is_disabled=True))
-    await state.clear() 
+    await callback.message.edit_text(
+        f"Твой вопрос отправлен!\n Жди ответа в чате по [ссылке]({UNDERGROUND_CHAT_INVITE}).",
+        parse_mode=ParseMode.MARKDOWN, link_preview_options=LinkPreviewOptions(is_disabled=True))
+
+    await state.clear()
+
 
 @dp.callback_query(F.data == "OK")
-async def idle_handler( state: FSMContext) -> None:
+async def idle_handler(state: FSMContext) -> None:
     await state.clear()
+
 
 # @dp.error(TelegramNetworkError)
 # async def on_network_error(error: TelegramNetworkError) -> None:
 #     await error.method.
 
+
 async def main() -> None:
     bot = Bot(TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     await tools.refresh()
-    await dp.start_polling(bot, polling_timeout=100,)
+    await dp.start_polling(bot, polling_timeout=100, )
 
 
 if __name__ == "__main__":
--- tools.py
+++ tools.py
@@ -3,36 +3,56 @@ import emoji_remover
 
 underground_chat_context = []
 
+
 async def add_new_question(question, answer):
     worksheet.append_row([question, answer], value_input_option="RAW")
 
+
 async def generate_short(message):
-        messages = [{"role":"system", "content":NON_FOUND_PROMPT},{"role":"user", "content":message}]
-
-        chat_response = openai_client.chat.completions.create(
-            model="gpt-4o-mini",
-            messages=messages,
-            temperature=0.5
-        )
-        response_text = chat_response.choices[0].message.content
-
-        return response_text
-
-async def find_question(message):   
-    current_search_prompt = SEARCH_PROMPT.format(enumerated_questions=enumerated_questions)
-    messages = [{"role":"system", "content":current_search_prompt},{"role":"user", "content":message}]
+    messages = [{"role":"system", "content":NON_FOUND_PROMPT}, {"role":"user", "content":message}]
 
     chat_response = openai_client.chat.completions.create(
-            model="gpt-4o-mini",
-            messages=messages,
-            temperature=0
-        )
+        model="gpt-4o-mini",
+        messages=messages,
+        temperature=0.5
+    )
     response_text = chat_response.choices[0].message.content
 
     return response_text
 
+
+async def generate_dialog(message):
+    messages = [{"role":"system", "content":NON_FOUND_PROMPT},
+                {"role":"message_history", "content":context_to_text()},
+                {"role":"user", "content":message}]
+
+    chat_response = openai_client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=messages,
+        temperature=0.5
+    )
+    response_text = chat_response.choices[0].message.content
+
+    return response_text
+
+
+async def find_question(message):
+    current_search_prompt = SEARCH_PROMPT.format(enumerated_questions=enumerated_questions)
+    messages = [{"role":"system", "content":current_search_prompt}, {"role":"user", "content":message}]
+
+    chat_response = openai_client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=messages,
+        temperature=0
+    )
+    response_text = chat_response.choices[0].message.content
+
+    return response_text
+
+
 def find_answer(number):
-    return answers[number-1]
+    return answers[number - 1]
+
 
 async def refresh():
     global df, questions, answers, enumerated_questions
@@ -40,17 +60,21 @@ async def refresh():
     answers = worksheet.col_values(2)
     enumerated_questions = '\n'.join([f"{index + 1}. {item}" for index, item in enumerate(questions)])
 
+
 async def mentioned(text):
     if any(mention in text.lower() for mention in MENTIONS):
         return True
 
-async def reply(text):
-        number = int(await find_question(text))
-        if number == 0:
-            response = await generate_short(text)
-        else:
-            response = find_answer(number)
-        return response
+
+async def reply(text, dialog_mode=False):
+    number = int(await find_question(text))
+    if number == 0:
+        response = await generate_short(text) if not dialog_mode else await generate_dialog(text)
+    else:
+        response = find_answer(number)
+
+    return response
+
 
 async def remove_names(input_string):
     for mention in MENTIONS:
@@ -58,20 +82,22 @@ async def remove_names(input_string):
 
     return input_string
 
+
 async def deepinfra_test(message):
-        messages = [{"role":"system", "content":NON_FOUND_PROMPT},{"role":"user", "content":message}]
+    messages = [{"role":"system", "content":NON_FOUND_PROMPT}, {"role":"user", "content":message}]
 
-        chat_response = deepinfra_client.chat.completions.create(
-            model=ANSWER_MODEL,
-            messages=messages,
-            temperature=0
-        )
-        response_text = chat_response.choices[0].message.content
+    chat_response = deepinfra_client.chat.completions.create(
+        model=ANSWER_MODEL,
+        messages=messages,
+        temperature=0
+    )
+    response_text = chat_response.choices[0].message.content
 
-        return response_text
+    return response_text
+
 
 async def summarize(text):
-    messages = [{"role":"system", "content":SUMMARIZE_PROMPT},{"role":"user", "content":text}]
+    messages = [{"role":"system", "content":SUMMARIZE_PROMPT}, {"role":"user", "content":text}]
 
     chat_response = openai_client.chat.completions.create(
         model="gpt-4o-mini",
@@ -82,25 +108,32 @@ async def summarize(text):
 
     return summary
 
+
 async def update_undergound_context(message, name=None):
     global undergound_chat_context
+
     if str(message.chat.id) == UNDERGROUND_CHAT_ID:
         if not name:
             underground_chat_context.append(message.from_user.first_name + ": " + message.text)
         else:
             underground_chat_context.append(name + ": " + message)
+
         if len(underground_chat_context) > 50:
             del underground_chat_context[0]
 
-async def formatted_reply(text):
-    reply = await reply(text)
-    formatted_response = await emoji_remover.rm(reply.lower())
+
+async def formatted_reply(text, dialog_mode=False):
+    reply_msg = await reply(text, dialog_mode)
+    formatted_response = await emoji_remover.rm(reply_msg.lower())
+
     return formatted_response
+
 
 async def is_admin(message):
     if message.from_user.username in ADMINS:
         return True
-    
+
+
 async def context_to_text():
     text_to_summarize = "\n".join(underground_chat_context)
     return text_to_summarize
